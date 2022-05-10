fbe6c1936aee7c0471f04c052112dc780f38dd94
duaa-braik <78284319+duaa-braik@users.noreply.github.com>
2022-05-10 03:16:22 +0300

    Coach

--- Coach.js
+++ Coach.js
@@ -1,14 +1,27 @@
-import React from "react";
+import logo from './logo.svg';
 import './App.css';
-import NavigationBar from "./navigationBar";
+import NavigationBar from './navigationBar';
+import { Bar } from 'react-chartjs-2';
+import Players from './players';
+import Filters from './filters';
 
-export default function A12() {
-    return (
-        <div>
-            
-            <h1>hello World</h1>
-        </div>
+export default function App() {
+  return (
+    <div className="App">
+      <NavigationBar />
+      <div className='contents'>
+        <Filters />
+        <Players />
+      </div>
+      <div className="numbers">
+          <button className="number">1</button>
+          <button className="number">2</button>
+          <button className="number">3</button>
+
+      </div>
+      
 
 
-    );
+    </div>
+  );
 }
